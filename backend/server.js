049ec527ea38c1e8606c39c20ad7fc11f92d8361
PGrabowski-9290 <92215276+PGrabowski-9290@users.noreply.github.com>
2022-06-21 20:11:23 +0200

    Add port to .env

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const recipesRoute = require('./routes/recipesRoute');
 const authRoute = require('./routes/authRoute');
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
